f5d52e4093fdfb6ee513e180affb2d19007048c3
Rajaguru A <162814363+Rajaguru1701@users.noreply.github.com>
2025-04-24 18:59:32 +0530

    Add files via upload

---
+++ Q9.java
@@ -0,0 +1,36 @@
+public class Q9 {
+    public static void main(String[] args) {
+        String s1 = "cat";
+        String s2 = "tree";
+        StringBuilder ans = new StringBuilder();
+        boolean flag = true;
+        if(s2.charAt(0) == s1.charAt(s1.length()-1))
+        {
+            flag = false;
+        }
+        if(!flag)
+        {
+            int n = s1.length()-1;
+            for(int i=0;i<n;i++)
+            {
+                ans.append(s1.charAt(i));
+            }
+            for(int i=0;i<s2.length();i++)
+            {
+                ans.append(s2.charAt(i));
+            }
+        }
+        else
+        {
+            for(int i=0;i<s1.length();i++)
+            {
+                ans.append(s1.charAt(i));
+            }
+            for(int i=0;i<s2.length();i++)
+            {
+                ans.append(s2.charAt(i));
+            }
+        }
+        System.out.println(ans.toString());
+    }
+}
