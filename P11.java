ac8f1708f75300bbeed10b8eb38fadb4174ff600
Rajaguru A <162814363+Rajaguru1701@users.noreply.github.com>
2025-04-25 20:15:50 +0530

    Create P11.java

---
+++ P11.java
@@ -0,0 +1,32 @@
+// Input: s = "Thisisdemostring", ch = 'i', count = 3
+// Output: ng
+// Explanation: The remaining substring of s after the 3rd
+// occurrence of 'i' is "ng", hence the output is ng.
+class P11 {
+    public static void main(String[] args) {
+        String s = "thisisdemostring";
+        int cnt = 3;
+        char c = 'i';
+        String res = isBe(s,cnt,c);
+        System.out.println(res);
+        
+    }
+    static String isBe(String s,int cnt,char c)
+    {
+        int Ccnt = 0; 
+        int j = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) == c)
+            {
+                Ccnt++;
+            }
+            else if(Ccnt == cnt)
+            {
+                return s.substring(j,s.length());  
+            }
+            j++;
+        }
+        return "";
+    }
+}
